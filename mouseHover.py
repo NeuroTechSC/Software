27184796f739937b431106d396b92203d7eb7ae4
Neville Hiramanek <nhiraman@ucsc.edu>
2022-04-30 15:07:50 -0700

    CSS fills whole display, hover win is smaller

--- mouseHover.py
+++ mouseHover.py
@@ -3,6 +3,9 @@ import webview
 import win32gui as wg
 import time
 
+width = 400
+height = 300
+
 def close_window(window,ogx,ogy):
   window.hide()
   while True:
@@ -12,18 +15,19 @@ def close_window(window,ogx,ogy):
     print(cursorStatus,x,y)
     if (cursorStatus == 65567):
       window.show()
-      window.move(x-400,y-300)
-      ogx, ogy = (x-400,y-300)
+      window.move(int(x-(width/2)),int(y-(height/2)))
+      ogx, ogy = (int(x-(width/2)),int(y-(height/2)))
       print("SHOW ME")
-    elif x < ogx or x > ogx + 800 or y < ogy or y > ogy + 600:
+    elif x < ogx or x > ogx + width or y < ogy or y > ogy + height:
       print ("Outside window")
       window.hide()
     else:
       print ("inside window")
 
 x,y = wg.GetCursorInfo()[2]
-current_win = webview.create_window('SSVEP Selector', 'http://127.0.0.1:5000',frameless=True,x=x-400,y=y-300)
-webview.start(func=close_window,args=(current_win,x-400,y-300),http_server=False)
+ogx,ogy = int(x-(width/2)), int(y-(height/2))
+current_win = webview.create_window('SSVEP Selector', 'http://127.0.0.1:5000',frameless=True,on_top=True,width=width,height=height,x=int(x-(width/2)),y=int(y-(height/2)))
+webview.start(func=close_window,args=(current_win,int(ogx),int(ogy)),http_server=False)
 
 
     
